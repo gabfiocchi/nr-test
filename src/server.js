924973692b0dd3a81be1b0fbfcfac617f525f82d
Gabriel Fiocchi <gab@thinkapp.studio>
2020-01-09 09:38:53 +0100

    Update server.js

--- src/server.js
+++ src/server.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.Server(app);
 
 app.use('/api', api);
-app.use('/', express.static(path.join(__dirname, '../dist')));
+app.use('/', express.static(path.join(__dirname, '../public')));
 
 // app.get('*', function (req, res) {
 //     res.status(404).send('Requested page not found')
