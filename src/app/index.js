ad3fdb5257605cb403c9ad680b58058c2c23c44c
Gabriel Fiocchi <gab@thinkapp.studio>
2020-01-13 06:23:41 -0300

    Update polyfills.

--- src/app/index.js
+++ src/app/index.js
@@ -1,5 +1,4 @@
 // eslint-disable-next-line import/no-extraneous-dependencies
-// import 'babel-polyfill';
 import './main.scss';
 import App from './core/app';
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,6 @@
 const path = require('path');
+// const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-
 
 module.exports = {
     entry: {
@@ -28,6 +28,10 @@ module.exports = {
         ]
     },
     plugins: [
+        // new webpack.ProvidePlugin({
+        //     Promise: 'es6-promise', // Thanks Aaron (https://gist.github.com/Couto/b29676dd1ab8714a818f#gistcomment-1584602)
+        //     fetch: 'imports?this=>global!exports?global.fetch!whatwg-fetch'
+        // }),
         new HtmlWebpackPlugin({
             hash: true,
             template: './src/app/index.html'
